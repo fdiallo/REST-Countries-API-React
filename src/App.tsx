e301eca233535a3c0133550ac9c46796bd22ab49
fdiallo <fallaye.diallo@gmail.com>
2026-05-12 19:40:02 -0400

    Implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:name" element={<Details />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App
